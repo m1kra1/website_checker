b6c5b08ffa18514229a0718286a348dc55df7ac3
m1kra1 <michael.kraj@hotmail.de>
2022-07-12 21:05:05 +0200

    Solved relativ filepath on desktop / Check if dir is existing - create

--- python/function.py
+++ python/function.py
@@ -1,10 +1,20 @@
 import os
 
-def pingURL(urlToCheck):
-    commandUrlToCheck = "ping "+ urlToCheck + " > C:/Users/MikeLaptop/Desktop/Output_{}.txt".format(urlToCheck)
+def userPathCheck(userPath):
+    #Check if dir exists
+    checkDir = os.path.isdir(userPath)
+    #If not exiting - create dir
+    if checkDir == False:
+        os.mkdir(userPath)
+    else:
+        return(userPath)
+
+def pingURL(urlToCheck, userFilePath):
+    #Get userFilePath and create Command for ping. Execute Command Line
+    commandUrlToCheck = "ping "+ urlToCheck + " > {}".format(userFilePath)
     print(commandUrlToCheck)
     os.system(commandUrlToCheck)
     
-def fileHandler(urlToCheck):
-        openFilePath = "C:/Users/MikeLaptop/Desktop/Output_{}.txt".format(urlToCheck)
-        os.system(openFilePath)
+    
+def fileHandler(userFilePath):
+        os.system(userFilePath)
--- python/main_handler.py
+++ python/main_handler.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 
 from requests import patch
-from function import pingURL, fileHandler
+from function import pingURL, fileHandler, userPathCheck
 import getpass
 
 
@@ -13,18 +13,19 @@ print("Die Ausgabe wird in einem File auf dem Desktop abgelegt.(Output_DeineURL)
 print("")
 urlToCheck = input("Welche URL soll gecheckt werden: ")
 
-#Creating relativ Path - not working 
-#userPath = "C:\users\{}\Desktop"
-#path = userPath.format(getpass.getuser())
-#print(path)
+#Create User Path to Desktop/Checker_Result/
+userPath = os.path.join(os.path.expandvars("%userprofile%"),"Desktop","checker_result")
+print(userPath)
+userPathCheck(userPath)
 
-pingURL(urlToCheck) #function.py - Putting the command line together - Store in File
+userFilePath = userPath + "/Output_{}.txt".format(urlToCheck)
+pingURL(urlToCheck, userFilePath) #function.py - Putting the command line together - Store in File
 
 
 
 openFile = input("Möchten Sie die Datei oeffnen? J/N: ").lower()
 
 if openFile == "j":
-    fileHandler(urlToCheck) #function.py - Check if file open requested - Commandline Path or Quit
+    fileHandler(userFilePath) #function.py - Check if file open requested - Commandline Path or Quit
 else:
     quit()
